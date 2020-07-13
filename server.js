c8cd3f4bf31dc481632d8e315231ed5362328728
akios dev <akiosdev6@gmail.com>
2020-07-13 15:26:55 -0300

    use env var to set host

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const HOST = process.env.HOST;
 
 let accessCount = 0;
 
-var whitelist = ['http://stereoblinddev.com', 'https://stereoblinddev.com'];
+var whitelist = [`http://${HOST}:3000`];
 var corsOptions = {
 	origin: function (origin, callback) {
 		if (whitelist.indexOf(origin) !== -1) {
+			console.log(origin);
 			callback(null, true);
 		} else {
 			callback('Not allowed by CORS');
